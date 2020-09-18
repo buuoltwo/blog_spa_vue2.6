31cee097de1af6e63e6a796cb18dd93f43879c31
conceit-sz <bullnick20@gmail.com>
2020-09-18 16:39:10 +0800

    主体css

---
+++ src/App2.vue
@@ -0,0 +1,50 @@
+<template>
+  <div id="app">
+    <Header id="header"/>
+    <main>
+      <router-view/>
+    </main>
+    <Footer id="footer"/>
+  </div>
+</template>
+
+<script>
+import Header from './components/Header'
+import Footer from '@/components/Footer'
+export default {
+  components: {
+    Header,
+    Footer
+  }
+}
+</script>
+<style lang="less">
+*, *::before, *::after {
+  margin: 0;
+}
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  // color: #2c3e50;
+
+  // display: flex;
+  // flex-direction: column;
+  min-height: 100vh;
+  display: grid;
+  grid: ~"auto 1fr auto / 12% 1fr 12%";
+}
+#header {
+  grid-area: ~"1/1/2/4";
+}
+#footer {
+  grid-area: ~"3/1/4/4";
+}
+main {
+  grid-area: ~"2/2/3/3";
+  // flex:1;
+}
+
+
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,116 @@
+<template>
+  <header v-bind:class="{login: isLogin, 'no-login': !isLogin}">
+    <template v-if="isTesting">
+      <p>header</p>
+      <div id="nav">
+        <router-link to="/">Home</router-link>||
+        <router-link to="/about">About</router-link>
+      </div>
+    </template>
+    <template v-if="!isLogin">
+      <h1>Let's share</h1>
+      <p>精品博客汇聚</p>
+      <div class="btns">
+        <router-link to="/login">
+          <el-button>立即登录</el-button>
+        </router-link>
+        <router-link to="/register">
+          <el-button>注册账号</el-button>
+        </router-link>
+      </div>
+    </template>
+    <template v-else>
+      <h1>Let's share</h1>
+      <i class="edit el-icon-edit"></i>
+      <!-- <div class="user">
+        <img class="avatar" :src="user.avatar" :alt="user.username" :title="user.username" />
+        <ul>
+          <li>
+            <router-link to="my">我的</router-link>
+          </li>
+          <li>
+            <a href="#" @click="onLogout">注销</a>
+          </li>
+        </ul>
+      </div> -->
+    </template>
+  </header>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      isTesting: false,
+      isLogin: true,
+    };
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="less" scoped>
+@import "../assets/base.less";
+
+#nav {
+  padding: 30px;
+
+  a {
+    font-weight: bold;
+    color: #2c3e50;
+
+    &.router-link-exact-active {
+      color: #42b983;
+    }
+  }
+}
+//
+#header.no-login {
+  background-color: @bgColor;
+  padding: 0 12% 30px 12%;
+  display: grid;
+  justify-items: center;
+
+  h1 {
+    color: #fff;
+    font-size: 40px;
+    margin: 60px 0 0 0;
+    text-transform: uppercase;
+  }
+
+  p {
+    margin: 15px 0 0 0;
+    color: #fff;
+  }
+
+  .btns {
+    margin-top: 20px;
+  }
+
+  button {
+    margin: 20px 5px 0;
+  }
+}
+//
+header.login {
+  display: flex;
+  align-items: center;
+  background: @bgColor;
+
+  h1 {
+    margin: 0;
+    padding: 0;
+    color: #fff;
+    font-size: 40px;
+    text-transform: uppercase;
+    flex: 1;
+  }
+
+  .edit {
+    color: #fff;
+    font-size: 30px;
+  }
+
+
+}
+</style>>
