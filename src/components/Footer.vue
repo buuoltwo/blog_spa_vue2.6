f14ccff32d3b05f377ccd605d50881016ae215ea
conceit-sz <bullnick20@gmail.com>
2020-09-21 22:15:01 +0800

    补充提交

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -13,5 +13,8 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="less">
-
+  p {
+    text-align: center;
+    font-weight: 200;
+  }
 </style>
