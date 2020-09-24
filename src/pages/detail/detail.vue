dbe3179041ac2e03d0aa247dfab770d9857fc8e9
conceit-sz <bullnick20@gmail.com>
2020-09-24 16:04:56 +0800

    /detail的user入口

--- src/pages/detail/detail.vue
+++ src/pages/detail/detail.vue
@@ -4,7 +4,7 @@
     <section class="user-info">
       <img :src="user.avatar" />
       <h3>{{title}}</h3>
-      <router-link :to="{ name:'user', params: {id:user.id} }"><p>{{user.username}}</p></router-link>
+      <p><router-link :to="{ name:'user', params: {id:user.id} }">{{user.username}}</router-link>更新于{{user.updatedAt}}</p>
     </section>
     <section class="article" v-html="markdown">文章本体</section>
   </div>
