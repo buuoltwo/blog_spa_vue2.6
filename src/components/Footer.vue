4c31747011d845c00da38792eb2ebe73af652bac
conceit-sz <bullnick20@gmail.com>
2020-09-25 11:35:56 +0800

    细节2

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,5 +1,7 @@
 <template>
-  <p>footer</p>
+  <div id="footer">
+    <p><span>技术栈Vue 2.6_</span>在线多人博客项目<router-link class="href" to="https://www.jianshu.com/u/64a0466a14e6">©buuotwo</router-link></p>
+  </div>
 </template>
 
 <script>
@@ -13,8 +15,23 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="less">
+@import url('../assets/base.less');
+#footer {
+  background-color: #d7d7d7;
+  color: #666;
+  padding: 10px;
+
   p {
     text-align: center;
     font-weight: 200;
+
+    & > span {
+      font-weight: lighter;
+      margin-right: 5px;
+    }
+    & > .href {
+      color: @bgColor;
+    }
+  }
   }
 </style>
