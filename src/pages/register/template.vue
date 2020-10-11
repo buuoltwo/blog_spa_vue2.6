fc60abe03dc40c7a51ab9fdd8d65b3c2c23d2ab3
buuoltwo <bullnick20@gmail.com>
2020-10-11 12:44:36 +0800

    移动端注册样式

--- src/pages/register/template.vue
+++ src/pages/register/template.vue
@@ -1,10 +1,24 @@
 <template>
   <div id="login">
-    <h4>用户名</h4>
-    <el-input v-model="username" placeholder="用户名" size="medium"></el-input>
-    <h4>密码</h4>
-    <el-input v-model="password" placeholder="密码" size="medium" @keyup.enter="onLogin" show-password></el-input>
-    <el-button size="medium" @click="onRegister">立即注册</el-button>
+    <fieldset>
+      <legend>请注册</legend>
+      <h4>用户名</h4>
+      <el-input
+        v-model="username"
+        placeholder="用户名"
+        size="medium"
+      ></el-input>
+      <h4>密码</h4>
+      <el-input
+        v-model="password"
+        placeholder="密码"
+        size="medium"
+        @keyup.enter="onLogin"
+        show-password
+      ></el-input>
+      <el-button size="medium" @click="onRegister">立即注册</el-button>
+    </fieldset>
+
     <!-- <p class="notice">
       没有账号？
       <router-link to="/register">注册新用户</router-link>
